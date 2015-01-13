a25f9652b4266067ed5fa92892dcb0ddc3cd0195
oeg8168 <oeg8168@gmail.com>
2015-01-13 20:08:22 +0800

    Combine core and GUI

--- src/GUI/MainApplication.java
+++ src/GUI/MainApplication.java
@@ -12,23 +12,40 @@ import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JSeparator;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.SwingConstants;
 import javax.swing.JSlider;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import core.CoreAlg;
+import dataIO.DataIO;
+import dataIO.NHIRD_CD_Data;
+import dataIO.NHIRD_OO_Data;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.awt.Color;
+
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableWorkbook;
 
 public class MainApplication {
 
 	private JFrame frmNhird;
+	private String CDFilePath;
+	private String OOFilePath;
+	private String outputPath;
 	private JTextField textFieldDiseaseCode;
 	private JTextField textFieldDrugCode;
 
@@ -60,6 +77,7 @@ public class MainApplication {
 	 */
 	private void initialize() {
 		frmNhird = new JFrame();
+		frmNhird.setResizable(false);
 		frmNhird.setTitle("NHIRD");
 		frmNhird.setBounds(100, 100, 600, 530);
 		frmNhird.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -208,6 +226,11 @@ public class MainApplication {
 		btnSubmit.setBounds(10, 432, 200, 50);
 		frmNhird.getContentPane().add(btnSubmit);
 
+		JLabel lblSubmitCheck = new JLabel("");
+		lblSubmitCheck.setForeground(Color.RED);
+		lblSubmitCheck.setBounds(220, 432, 260, 50);
+		frmNhird.getContentPane().add(lblSubmitCheck);
+
 		JButton btnAbout = new JButton("About");
 		btnAbout.setBounds(484, 452, 90, 30);
 		frmNhird.getContentPane().add(btnAbout);
@@ -220,6 +243,7 @@ public class MainApplication {
 				chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 				if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
 					System.out.println(chooser.getSelectedFile().getAbsolutePath());
+					CDFilePath = chooser.getSelectedFile().getAbsolutePath();
 					lblCDFilePath.setText("CD file path: " + chooser.getSelectedFile().getAbsolutePath());
 				}
 			}
@@ -231,6 +255,7 @@ public class MainApplication {
 				chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 				if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
 					System.out.println(chooser.getSelectedFile().getAbsolutePath());
+					OOFilePath = chooser.getSelectedFile().getAbsolutePath();
 					lblOOFilePath.setText("OO file path: " + chooser.getSelectedFile().getAbsolutePath());
 				}
 			}
@@ -246,7 +271,7 @@ public class MainApplication {
 				}
 			}
 		});
-		
+
 		lblLink.setForeground(Color.BLUE);
 		lblLink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		lblLink.addMouseListener(new MouseAdapter() {
@@ -262,6 +287,108 @@ public class MainApplication {
 					}
 				}
 
+			}
+		});
+
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				boolean passFlag = true;
+				String alertString = new String("<html>");
+
+				if (OOFilePath == null || CDFilePath == null) {
+					alertString += "Please select CD/OO file!<br>";
+					passFlag = false;
+				}
+
+				if (textFieldDiseaseCode.getText().isEmpty() && textFieldDrugCode.getText().isEmpty()) {
+					alertString += "Please enter query! (Disease or Drug)<br>";
+					passFlag = false;
+				}
+
+				if (!passFlag) {
+					alertString += "</html>";
+					lblSubmitCheck.setText(alertString);
+
+				} else {
+					lblSubmitCheck.setText("Processing...");
+
+					NHIRD_OO_Data OO = new NHIRD_OO_Data(new File(OOFilePath));
+					NHIRD_CD_Data CD = new NHIRD_CD_Data(new File(CDFilePath));
+
+					HashSet<String> diseases = CoreAlg.parseQuery(textFieldDiseaseCode.getText());
+					HashSet<String> drugs;
+
+					File drugFile = new File(textFieldDrugCode.getText());
+					if (drugFile.isFile()) {
+						drugs = DataIO.readDrugFile(drugFile);
+					} else {
+						drugs = CoreAlg.parseQuery(textFieldDrugCode.getText());
+					}
+
+					HashSet<String> queryResult = CoreAlg.query(CD, OO, diseases, drugs);
+
+					String[] sortingOrder = { comboBox_1.getSelectedItem().toString(), comboBox_2.getSelectedItem().toString(), comboBox_3.getSelectedItem().toString(), comboBox_4.getSelectedItem().toString() };
+
+					ArrayList<String> sortedKeys = CoreAlg.sortByOrder(CD, OO, queryResult, sortingOrder);
+					
+					JFileChooser chooser = new JFileChooser();
+					chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+					if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+						System.out.println(chooser.getSelectedFile().getAbsolutePath());
+						outputPath = chooser.getSelectedFile().getAbsolutePath();
+					}
+					
+					try {
+						WritableWorkbook workbook = Workbook.createWorkbook(new File(outputPath + "/output.xls"));
+						
+						workbook.createSheet("result", 0);
+						
+						workbook.getSheet(0).addCell(new Label(0,0,"ID"));
+						workbook.getSheet(0).addCell(new Label(1,0,"ID_SEX"));
+						workbook.getSheet(0).addCell(new Label(2,0,"ID_BIRTHDAY"));
+						workbook.getSheet(0).addCell(new Label(3,0,"ACODE_ICD9_1"));
+						workbook.getSheet(0).addCell(new Label(4,0,"ACODE_ICD9_2"));
+						workbook.getSheet(0).addCell(new Label(5,0,"ACODE_ICD9_3"));
+						workbook.getSheet(0).addCell(new Label(6,0,"DRUG_NO"));
+						workbook.getSheet(0).addCell(new Label(7,0,"DRUG_DAY"));
+						workbook.getSheet(0).addCell(new Label(8,0,"TOTAL_QTY"));
+						workbook.getSheet(0).addCell(new Label(9,0,"FUNC_DATE"));
+						
+						int row = 1;
+						for (String key : sortedKeys) {
+							
+							workbook.getSheet(0).addCell(new Label(0,row,CD.getItem(key, "ID")));
+							workbook.getSheet(0).addCell(new Label(1,row,CD.getItem(key, "ID_SEX")));
+							workbook.getSheet(0).addCell(new Label(2,row,CD.getItem(key, "ID_BIRTHDAY")));
+							workbook.getSheet(0).addCell(new Label(3,row,CD.getItem(key, "ACODE_ICD9_1")));
+							workbook.getSheet(0).addCell(new Label(4,row,CD.getItem(key, "ACODE_ICD9_2")));
+							workbook.getSheet(0).addCell(new Label(5,row,CD.getItem(key, "ACODE_ICD9_3")));
+							workbook.getSheet(0).addCell(new Label(6,row,OO.getItem(key, "DRUG_NO")));
+							workbook.getSheet(0).addCell(new Label(7,row,CD.getItem(key, "DRUG_DAY")));
+							workbook.getSheet(0).addCell(new Label(8,row,OO.getItem(key, "TOTAL_QTY")));
+							workbook.getSheet(0).addCell(new Label(9,row,CD.getItem(key, "FUNC_DATE")));
+							
+							row++;
+						}
+						
+						workbook.write();
+						workbook.close();
+						
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					lblSubmitCheck.setText("");
+				}
+
+			}
+		});
+
+		btnAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(frmNhird, "<html>aaa<br>ddd</html>", "Title", JOptionPane.PLAIN_MESSAGE);
 			}
 		});
 
